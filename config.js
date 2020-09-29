c922728b848681b62c66c385e0e871905476c7f3
Thieu Anh <anhtd@pexdia.com>
2020-09-29 09:26:12 +0700

    a

---
+++ config.js
@@ -0,0 +1,3 @@
+const url = "mongodb://root:NDwnMEJ09pds@52.53.199.155:27017/datvt?connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256&3t.uriVersion=3&3t.connection.name=tunel-app";
+
+module.exports = {url}
--- service.js
+++ service.js
@@ -16,15 +16,119 @@ async function bestByOrderToday() {
         .sort({
             last_order: -1
         });
-    console.log("bestByOrderToday Size:", results.length)
+    console.log("Best By Order Today 100 Size:", results.length)
     results = results.map(elm => `${elm.post_id}`).join(',')
-    await fs.writeFile('bestByOrderToday.txt', results, 'utf8', function (err) {
+    await fs.writeFile('bestByOrderToday100.txt', results, 'utf8', function (err) {
         if (err) {
             console.log("An error occured while writing JSON Object to File.", err);
         }
     });
-    return results
+    results = await Ads.find({
+        orders: {
+            $not: {
+                $elemMatch: {
+                    statistical_time: { $lt: moment.utc().startOf('day').toDate() }
+                }
+            }
+        },
+        last_order: { $lt: 100, $gte: 50 },
+    }, { post_id: 1 })
+        .sort({
+            last_order: -1
+        });
+    console.log("Best By Order Today 50 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByOrderToday50.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
 
+    results = await Ads.find({
+        orders: {
+            $not: {
+                $elemMatch: {
+                    statistical_time: { $lt: moment.utc().startOf('day').toDate() }
+                }
+            }
+        },
+        last_order: { $lt: 50, $gte: 10 },
+    }, { post_id: 1 })
+        .sort({
+            last_order: -1
+        });
+    console.log("Best By Order Today 10 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByOrderToday10.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
 }
 
-module.exports = { bestByOrderToday }
+async function bestByOrderYesterday() {
+    let results = await Ads.find({
+        orders: {
+            $not: {
+                $elemMatch: {
+                    statistical_time: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() }
+                }
+            }
+        },
+        last_yesterday: { $lt: 9999, $gte: 100 },
+    }, { post_id: 1 })
+        .sort({
+            last_yesterday: -1
+        });
+    console.log("Best By Order Yesterday 100 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByOrderYesterday100.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+    results = await Ads.find({
+        orders: {
+            $not: {
+                $elemMatch: {
+                    statistical_time: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() }
+                }
+            }
+        },
+        last_yesterday: { $lt: 100, $gte: 50 },
+    }, { post_id: 1 })
+        .sort({
+            last_yesterday: -1
+        });
+    console.log("Best By Order Yesterday 50 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByOrderYesterday50.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+
+    results = await Ads.find({
+        orders: {
+            $not: {
+                $elemMatch: {
+                    statistical_time: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() }
+                }
+            }
+        },
+        last_yesterday: { $lt: 50, $gte: 10 },
+    }, { post_id: 1 })
+        .sort({
+            last_yesterday: -1
+        });
+
+    console.log("Best By Order Yesterday 10 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByOrderYesterday10.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+}
+
+module.exports = { bestByOrderToday, bestByOrderYesterday }
