7e6c0d0e48a9896770296ab2adfbe63b5570f098
Thieu Anh <anhtd@pexdia.com>
2020-09-29 15:08:56 +0700

    a

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const service = require('./service')
 var mongoose = require('mongoose');
-const url = require('./config')
+const { url } = require('./config')
 
 const start = async () => {
   await mongoose.connect(url, { useNewUrlParser: true });
   const db = mongoose.connection;
   await service.bestByOrderToday();
   await service.bestByOrderYesterday()
+  await service.bestByReactToday()
+  await service.bestByReactYesterday()
+
   db.close()
 }
 
--- service.js
+++ service.js
@@ -131,4 +131,167 @@ async function bestByOrderYesterday() {
     });
 }
 
-module.exports = { bestByOrderToday, bestByOrderYesterday }
+async function bestByReactToday() {
+    // 7000
+    let results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $gte: 7000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Today 7000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactToday7000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+    // 3000
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 7000, $gte: 3000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Today 3000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactToday3000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+    // 1000
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 3000, $gte: 1000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Today 1000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactToday1000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+
+    // 500
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 1000, $gte: 500 },
+        shares_reactions: { $gte: 0.2 }
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+
+    console.log("Best By React Today 500 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactToday500.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+}
+
+async function bestByReactYesterday() {
+    // 7000
+    let results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $gte: 7000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Yesterday 7000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactYesterday7000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+    // 3000
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 7000, $gte: 3000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Yesterday 3000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactYesterday3000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+    // 1000
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 3000, $gte: 1000 },
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+    console.log("Best By React Yesterday 1000 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactYesterday1000.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+
+    // 500
+    results = await Ads.find({
+        tracking_time_arr: {
+            $not: { $elemMatch: { $lt: moment.utc().startOf('day').subtract(1, "days").toDate() } }
+        },
+        action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+        reactions: { $lt: 1000, $gte: 500 },
+        shares_reactions: { $gte: 0.2 }
+    }, { post_id: 1 })
+        .sort({
+            reactions: -1
+        });
+
+
+    console.log("Best By React Yesterday 500 Size:", results.length)
+    results = results.map(elm => `${elm.post_id}`).join(',')
+    await fs.writeFile('bestByReactYesterday500.txt', results, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.", err);
+        }
+    });
+}
+module.exports = { bestByOrderToday, bestByOrderYesterday, bestByReactToday, bestByReactYesterday }
