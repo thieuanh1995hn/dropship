36abbf56b58e37a12faca1022b36654c73b8f371
Thieu Anh <anhtd@pexdia.com>
2020-10-02 08:39:23 +0700

    a

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const { url } = require('./config')
+const { db_url } = require('./config')
 const port = 3000
 const { app } = require('./router')
 require('./sequence')
 
 const start = async () => {
-  await mongoose.connect(url, { useNewUrlParser: true });
+  await mongoose.connect(db_url, { useNewUrlParser: true });
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
   app.listen(port, () => {
