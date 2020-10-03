72262ce8ab58d75bad6eeef0b637cd6abab49ced
Leslie <thieuanh1995hn@gmail.com>
2020-10-04 06:22:14 +0700

    a

--- model.js
+++ model.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-const adsSchema = new mongoose.Schema({ post_id: String });
+const adsSchema = new mongoose.Schema(
+    {
+        post_id: String
+    }
+);
 const Ads = mongoose.model('ads', adsSchema);
 
 module.exports = { Ads }
--- sequence.js
+++ sequence.js
@@ -1,18 +1,21 @@
 const service = require('./service')
 const axios = require('axios')
 const config = require('./config')
+const { Ads } = require('./model')
+const moment = require('moment')
+
 let orderToday = null
+let reactToday = null
 
 async function alertNewBestByOrderToday() {
     try {
         let r = await service.bestByOrderToday()
         r.order100 = r.order100.split(',')
         r.order50 = r.order50.split(',')
-        r.order10 = r.order10.split(',')
         if (orderToday) {
-            _old = [...orderToday.order100, ...orderToday.order50, ...orderToday.order10]
+            _old = [...orderToday.order100, ...orderToday.order50]
             _new = []
-            for (let post_id of [...r.order100, ...r.order50, ...r.order10]) {
+            for (let post_id of [...r.order100, ...r.order50]) {
                 if (!_old.includes(post_id)) {
                     _new.push(post_id)
                 }
@@ -35,11 +38,49 @@ async function alertNewBestByOrderToday() {
             }
         }
         orderToday = r
-        setTimeout(alertNewBestByOrderToday, 300000);
+        setTimeout(alertNewBestByOrderToday, 1800000);
     } catch (e) {
         console.error(e.stack)
-        setTimeout(alertNewBestByOrderToday, 300000);
+        setTimeout(alertNewBestByOrderToday, 1800000);
     }
 }
 
+async function alertReactCrawlDone() {
+    try {
+        let r = await Ads.find({
+            tracking_time_arr: {
+                $not: { $elemMatch: { $lt: moment.utc().startOf('day').toDate() } }
+            },
+            action: { $in: ["SHOP_NOW", "LEARN_MORE", "GET_OFFER"] },
+            reactions: { $gte: 7000 },
+        }, { post_id: 1 })
+            .sort({
+                reactions: -1
+            });
+        r = r.map(elm => `${elm.post_id}`).join(',')
+
+        //TODO
+        if (reactToday !== r) {
+            await axios.post("https://slack.com/api/chat.postMessage", {
+                channel: config.slackBotChanel,
+                text: `Sếp ơi react có rồi ạ!`,
+                username: 'Đệ tử ruột',
+                icon_url: "https://ca.slack-edge.com/T01CH5MVANL-U01BTC2K7L2-0a745d8aaeed-48"
+            }, {
+                headers: {
+                    "Authorization": config.slackBotAPIkey,
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+        reactToday = r
+        setTimeout(alertReactCrawlDone, 1800000);
+    } catch (e) {
+        console.error(e.stack)
+        setTimeout(alertReactCrawlDone, 1800000);
+    }
+
+}
+
 alertNewBestByOrderToday();
+alertReactCrawlDone();
